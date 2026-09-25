411423a383c01024028b96dbdcad5c7699b4a4ff
Clément Berthieu <lumentoo.brh@gmail.com>
2026-09-25 10:22:53 +0200

    feat: ajout de rapport.py et configuration alternative

---
+++ config.py
@@ -0,0 +1,3 @@
+# config.py
+NOM_ARENE = "Stadium Occitanie"
+MAX_TOURS = 25
---
+++ rapport.py
@@ -0,0 +1,3 @@
+# rapport.py
+def afficher_score(victoires_a: int, victoires_b: int):
+    return f"Score : {victoires_a} - {victoires_b}"
