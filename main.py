0e50b019ea4dcc5786a48c5d867cb749a64437c9
Clément Berthieu <lumentoo.brh@gmail.com>
2026-09-25 10:55:33 +0200

    ci: ajout main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+from arene import lancer_duel
+from config import NOM_ARENE
+
+print(f"Bienvenue au {NOM_ARENE}")
+# ERREUR DE SYNTAXE VOLONTAIRE CI-DESSOUS (parenthèse non fermée) :
+print("Debut du tournoi"
