f8e4e9add083e5d110b943fd533558f30a699f53
Clément Berthieu <lumentoo.brh@gmail.com>
2026-09-25 11:16:23 +0200

    fix: ajout de la parenthèse

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from config import NOM_ARENE
 
 print(f"Bienvenue au {NOM_ARENE}")
 # ERREUR DE SYNTAXE VOLONTAIRE CI-DESSOUS (parenthèse non fermée) :
-print("Debut du tournoi"
+print("Debut du tournoi")
