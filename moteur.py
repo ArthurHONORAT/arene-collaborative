e9b544c5c739a01755fec95c6496efa01f169b35
Clément Berthieu <lumentoo.brh@gmail.com>
2026-09-25 08:52:57 +0200

    feat: calcul d'initiative selon la vitesse

---
+++ moteur.py
@@ -0,0 +1,18 @@
+#moteur.py
+def initialiser_arene(nom :str):
+    return {"nom": nom, "combattants": []}
+
+def attaque_charge():
+    return "Charge inflige 20 dégâts bruts"
+
+def boire_potion(points: int = 30):
+    return f"Soin de {points} PV"
+
+
+print("Moteur de jeu chargé.")
+print("Système prêt.")
+
+def determiner_initiative(vitesse_a: int, vitesse_b: int) -> str:
+    if vitesse_a >= vitesse_b:
+        return "combattant_a"
+    return "combattant_b"
